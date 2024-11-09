5771490a6e38ffe22f6532773b98e89114bab168
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-10 07:14:22 +0800

    Create proxy_list_tmp.js

---
+++ proxy_list_tmp.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * 示例： 
+ * export const proxyList = [
+    "http://proxyUser:proxyPass@proxyHost:proxyPort",
+    "http://proxyUser:proxyPass@proxyHost:proxyPort",
+    "http://proxyUser:proxyPass@proxyHost:proxyPort",
+  ];
+ */
+
+export const proxyList = []; // 代理列表
