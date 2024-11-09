e1fb41591ed813a9a686735753be67c38081a8c2
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-10 06:43:16 +0800

    Create account_tmp.js

---
+++ account_tmp.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * export const account = [
+ *   "BACKPACK WALLET 账户私钥",
+ *   "BACKPACK WALLET 账户私钥",
+ *   "BACKPACK WALLET 账户私钥",
+ * ];
+ *
+ */
+
+export const account = [
+    "BACKPACK WALLET 账户私钥",
+];
