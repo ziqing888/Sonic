02a40671223088a988d7f58955961ddec90b4c97
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-19 10:04:13 +0800

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -3,31 +3,33 @@ import logger from "../utils/logger.js";
 import { HttpsProxyAgent } from "https-proxy-agent";
 import fetch from "node-fetch";
 
-// API 类
 export class API {
   constructor(url, proxy) {
-    this.url = url; // 基础 URL
-    this.ua = Helper.randomUserAgent(); // 随机生成的用户代理
-    this.proxy = proxy; // 代理服务器地址
-    this.agent = this.proxy ? new HttpsProxyAgent(this.proxy) : undefined; // 创建代理对象
+    this.url = url;
+    this.ua = Helper.randomUserAgent();
+    this.proxy = this.proxy;
+    if (this.proxy) {
+      this.agent = new HttpsProxyAgent(this.proxy);
+    } else {
+      this.agent = undefined;
+    }
   }
 
-  // 生成请求头
   generateHeaders(token) {
     const headers = {
       Accept: "*/*",
       "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
       "Content-Type": "application/json",
-      "User-Agent": this.ua, // 用户代理
+      "User-Agent": this.ua,
     };
 
-    if (token) {
-      headers.Authorization = token; // 如果有令牌，则添加到请求头
+    if (this.token) {
+      headers.Authorization = token;
     }
+    // console.log(headers);
     return headers;
   }
 
-  // 发送请求
   async fetch(
     endpoint,
     method,
@@ -38,9 +40,9 @@ export class API {
     customUrl = undefined
   ) {
     try {
-      const url = `${customUrl === undefined ? this.url : customUrl}${endpoint}`;
+      const url = `${customUrl == undefined ? this.url : customUrl}${endpoint}`;
       let headers = this.generateHeaders(token);
-      headers = Object.assign(headers, additionalHeader); // 合并请求头
+      headers = Object.assign(headers, additionalHeader);
       const options = {
         cache: "default",
         credentials: cred,
@@ -53,27 +55,28 @@ export class API {
         referrerPolicy: "strict-origin-when-cross-origin",
       };
 
-      if (method !== "GET" && Object.keys(body).length > 0) {
-        options.body = JSON.stringify(body); // 如果不是 GET 请求且 body 不为空，则设置请求体
+      if (method !== "GET") {
+        options.body = `${JSON.stringify(body)}`;
       }
 
       logger.info(`${method} : ${url}`);
-      logger.debug(`请求头 : ${JSON.stringify(headers)}`);
-      logger.debug(`请求体 : ${JSON.stringify(body)}`);
+      logger.info(`Request Header : ${JSON.stringify(headers)}`);
+      logger.info(`Request Body : ${JSON.stringify(body)}`);
 
       const res = await fetch(url, options);
 
-      logger.info(`响应 : ${res.status} ${res.statusText}`);
+      logger.info(`Response : ${res.status} ${res.statusText}`);
+      logger.info(``);
 
-      if (res.ok || res.status === 400) {
+      if (res.ok || res.status == 400) {
         const data = await res.json();
-        logger.debug(`响应数据 : ${JSON.stringify(data)}`);
+        logger.info(`Response Data : ${JSON.stringify(data)}`);
         return data;
       } else {
-        throw new Error(`请求失败: ${res.status} ${res.statusText} (URL: ${url})`);
+        throw new Error(res.statusText);
       }
     } catch (err) {
-      logger.error(`错误 : ${err.message}`);
+      logger.error(`Error : ${err.message}`);
       throw err;
     }
   }
